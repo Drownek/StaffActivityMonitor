e82a3dfa731b95358d771d5382c8096ba7651a7e
Piotr <piotrgamingyt@gmail.com>
2025-07-10 19:57:22 +0200

    Modularize project structure and move Bukkit-related code to new submodule

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,136 +1,31 @@
-import xyz.jpenilla.runpaper.task.RunServer
-import java.util.Properties
-
 plugins {
     id("java")
-    id("net.minecrell.plugin-yml.bukkit") version "0.6.0"
-    id("com.gradleup.shadow") version "9.0.0-beta12"
-    id("xyz.jpenilla.run-paper") version "2.3.1"
 }
 
-group = "me.drownek"
-version = "1.0-SNAPSHOT"
+subprojects {
+    apply(plugin = "java")
+    apply(plugin = "maven-publish")
 
-bukkit {
-    main = "me.drownek.staffactivity.StaffActivityPlugin"
-    apiVersion = "1.13"
-    name = "StaffActivityMonitor"
-    author = "Drownek"
-    version = "${project.version}"
-}
+    group = "me.drownek"
+    version = "1.0-SNAPSHOT"
 
-repositories {
-    mavenCentral()
-    mavenLocal()
-    maven("https://jitpack.io")
-    maven("https://storehouse.okaeri.eu/repository/maven-public/")
-    maven("https://repo.panda-lang.org/releases")
-    maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
-}
-
-dependencies {
-    compileOnly("org.spigotmc:spigot-api:1.16.5-R0.1-SNAPSHOT")
-
-    implementation("com.github.Drownek:light-platform:2.0.1")
-
-    /* lombok */
-    val lombok = "1.18.32"
-    compileOnly("org.projectlombok:lombok:$lombok")
-    annotationProcessor("org.projectlombok:lombok:$lombok")
-}
-
-tasks.shadowJar {
-    archiveFileName.set("StaffActivityMonitor-${project.version}.jar")
-
-    exclude(
-        "org/intellij/lang/annotations/**",
-        "org/jetbrains/annotations/**",
-        "META-INF/**",
-        "javax/**"
-    )
-
-    val prefix = "me.drownek.staffactivity.libs"
-    listOf(
-        "eu.okaeri",
-        "dev.rollczi.litecommands",
-        "com.cryptomorin",
-        "dev.triumphteam",
-        "panda",
-        "net.jodah",
-        "net.kyori",
-        "me.drownek.util",
-    ).forEach { pack ->
-        relocate(pack, "$prefix.$pack")
-    }
-
-    /* Fail as it wont work on server versions with plugin remapping */
-    duplicatesStrategy = DuplicatesStrategy.FAIL
-}
-
-java {
-    toolchain {
-        languageVersion.set(JavaLanguageVersion.of(16))
-    }
-}
-
-tasks.withType<JavaCompile> {
-    options.compilerArgs.add("-parameters")
-    options.encoding = "UTF-8"
-}
-
-val runVersions = mapOf(
-    "1.8.8" to 16,
-    "1.16.5" to 16,
-    "1.18.2" to 17,
-    "1.19.4" to 19,
-    "1.20.6" to 21,
-    "1.21.5" to 21,
-    "1.21.6" to 21,
-    "1.21.7" to 21,
-)
-
-tasks {
-    runVersions.forEach { key, value ->
-        val n = key.replace(".", "_")
-        register("run$n", RunServer::class) {
-            minecraftVersion(key)
-
-            /* Automatically accept EULA */
-            jvmArgs("-Dcom.mojang.eula.agree=true")
-
-            val runDir = layout.projectDirectory.dir("run$n")
-            runDirectory.set(runDir)
-            pluginJars.from(shadowJar.flatMap { it.archiveFile })
-
-            /* Start server with specified Java version */
-            val toolchains = project.extensions.getByType<JavaToolchainService>()
-            javaLauncher.set(toolchains.launcherFor {
-                languageVersion.set(JavaLanguageVersion.of(value))
-            })
-
-            /* Assign random port for multiple instances at the same time */
-            doFirst {
-                val runDirFile = runDir.asFile
-                if (!runDirFile.exists()) {
-                    runDirFile.mkdirs()
-                }
-
-                val serverPropertiesFile = runDirFile.resolve("server.properties")
-                if (!serverPropertiesFile.exists()) {
-                    serverPropertiesFile.createNewFile()
-                }
-
-                val props = Properties().apply {
-                    serverPropertiesFile.inputStream().use { load(it) }
-                }
-
-                val randomPort = (20000..40000).random()
-                props["server-port"] = randomPort.toString()
-
-                serverPropertiesFile.outputStream().use { props.store(it, null) }
-
-                println(">> Starting server $key on port $randomPort")
-            }
+    java {
+        toolchain {
+            languageVersion.set(JavaLanguageVersion.of(16))
         }
+    }
+
+    tasks.withType<JavaCompile> {
+        options.compilerArgs.add("-parameters")
+        options.encoding = "UTF-8"
+    }
+
+    repositories {
+        mavenCentral()
+        mavenLocal()
+        maven("https://jitpack.io")
+        maven("https://storehouse.okaeri.eu/repository/maven-public/")
+        maven("https://repo.panda-lang.org/releases")
+        maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,3 +3,4 @@ plugins {
     // add toolchain resolver
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.9.0"
 }
+include("bukkit")

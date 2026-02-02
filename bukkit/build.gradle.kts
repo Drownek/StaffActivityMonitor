1dc29f4a6d392fd165998c9d50574373b920ef11
Piotr <piotrgamingyt@gmail.com>
2026-02-02 19:29:53 +0100

    update e2e test dependencies to use paper-e2e-runner version 1.0.5

--- bukkit/build.gradle.kts
+++ bukkit/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
     id("net.minecrell.plugin-yml.bukkit") version "0.6.0"
     id("com.gradleup.shadow") version "9.0.0-beta12"
     id("xyz.jpenilla.run-paper") version "2.3.1"
-    id("io.github.drownek.paper-e2e") version "1.0.4"
+    id("io.github.drownek.paper-e2e") version "1.0.5"
 }
 
 e2e {
